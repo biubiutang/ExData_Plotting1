d15adcaca81550ef2be46b4df1d7640db90e4c10
biubiutang <349731289@qq.com>
2015-07-12 16:52:22 +0800

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,33 @@
+dataset<-read.table("~/Downloads/household_power_consumption.txt",header = T,sep = ";")
+dataset$Date<-as.Date(dataset$Date,"%d/%m/%Y")
+data<-subset(dataset,Date=="2007-02-01"|Date=="2007-02-02")
+#change the format of the numbers
+data$Global_active_power<-as.character(data$Global_active_power)
+data$Global_active_power<-as.numeric(data$Global_active_power)
+data$Sub_metering_1<-as.character(data$Sub_metering_1)
+data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
+data$Sub_metering_2<-as.character(data$Sub_metering_2)
+data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
+data$Voltage<-as.character(data$Voltage)
+data$Voltage<-as.numeric(data$Voltage)
+data$Global_reactive_power<-as.character(data$Global_reactive_power)
+data$Global_reactive_power<-as.numeric(data$Global_reactive_power)
+#paint pictures
+png(file="plot4.png")
+par(mfcol=c(2,2),mar=c(4,4,1,1))
+
+plot(data$Global_active_power,type = "l",ylab = "Global Active Power",xlab = "",xaxt = "n")
+axis(side = 1,at = c(0,1500,2900), labels = c("Thu","Fri","Sat"))
+
+plot(data$Sub_metering_1,col="black",type="l",xlab="",ylab="Energy sub metering",xaxt="n")
+axis(side = 1,at = c(0,1500,2900), labels = c("Thu","Fri","Sat"))
+lines(data$Sub_metering_2,col="red",type="l",xaxt="n")
+lines(data$Sub_metering_3,col="blue",type="l",xaxt="n")
+legend("topright",pch="-",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+
+plot(data$Voltage,type="l",xlab="datetime",ylab="Voltage",xaxt="n")
+axis(side = 1,at = c(0,1500,2900), labels = c("Thu","Fri","Sat"))
+
+plot(data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power",xaxt="n")
+axis(side = 1,at = c(0,1500,2900), labels = c("Thu","Fri","Sat"))  
+dev.off()

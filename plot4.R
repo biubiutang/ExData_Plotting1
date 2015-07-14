37e2a14c5c186523ae0b433e38a2dd8ca7dd8c10
biubiutang <biubiutang@tangyinandeMacBook-Air.local>
2015-07-14 14:47:41 +0800

    corrected

--- plot4.R
+++ plot4.R
@@ -1,6 +1,7 @@
 dataset<-read.table("~/Downloads/household_power_consumption.txt",header = T,sep = ";")
 dataset$Date<-as.Date(dataset$Date,"%d/%m/%Y")
 data<-subset(dataset,Date=="2007-02-01"|Date=="2007-02-02")
+data$Time<-strptime(paste(data$Date,data$Time),format = "%Y-%m-%d %H:%M:%OS")
 #change the format of the numbers
 data$Global_active_power<-as.character(data$Global_active_power)
 data$Global_active_power<-as.numeric(data$Global_active_power)
@@ -16,18 +17,14 @@ data$Global_reactive_power<-as.numeric(data$Global_reactive_power)
 png(file="plot4.png")
 par(mfcol=c(2,2),mar=c(4,4,1,1))
 
-plot(data$Global_active_power,type = "l",ylab = "Global Active Power",xlab = "",xaxt = "n")
-axis(side = 1,at = c(0,1500,2900), labels = c("Thu","Fri","Sat"))
+plot(data$Time,data$Global_active_power,type = "l",ylab = "Global Active Power",xlab = "")
 
-plot(data$Sub_metering_1,col="black",type="l",xlab="",ylab="Energy sub metering",xaxt="n")
-axis(side = 1,at = c(0,1500,2900), labels = c("Thu","Fri","Sat"))
-lines(data$Sub_metering_2,col="red",type="l",xaxt="n")
-lines(data$Sub_metering_3,col="blue",type="l",xaxt="n")
-legend("topright",pch="-",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
+plot(data$Time,data$Sub_metering_1,col="black",type="l",xlab="",ylab="Energy sub metering")
+lines(data$Time,data$Sub_metering_2,col="red",type="l")
+lines(data$Time,data$Sub_metering_3,col="blue",type="l")
+legend("topright",lty="solid",col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"))
 
-plot(data$Voltage,type="l",xlab="datetime",ylab="Voltage",xaxt="n")
-axis(side = 1,at = c(0,1500,2900), labels = c("Thu","Fri","Sat"))
+plot(data$Time,data$Voltage,type="l",xlab="datetime",ylab="Voltage")
 
-plot(data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power",xaxt="n")
-axis(side = 1,at = c(0,1500,2900), labels = c("Thu","Fri","Sat"))  
+plot(data$Time,data$Global_reactive_power,type="l",xlab="datetime",ylab="Global_reactive_power")
 dev.off()
